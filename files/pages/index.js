aed1830140d6bbd0dd083c8501713e2bd87a5be7
Sarinna Sung <ssung101@bu.edu>
2021-08-19 19:45:10 -0400

    Initial template

--- files/pages/index.js
+++ files/pages/index.js
@@ -1,35 +1,16 @@
 import React from 'react';
-import Layout from "../components/Layout";
-import NavOne from "../components/NavOne";
+
 import Footer from "../components/Footer";
-import SliderOne from "../components/SliderOne";
-import CampaignArea from "../components/CampaignArea";
-import ServiceArea from "../components/ServiceArea";
-import MixerArea from "../components/MixerArea";
-import CallToAction from "../components/CallToAction";
-
-import CallToActionTwo from "../components/CallToActionTwo";
-
-import ExamplePaypal from '../components/ExamplePaypal';
-import GoodPartners from '../components/GoodPartners';
-import RaisingMoney from '../components/RaisingMoney';
-import GraphOne from '../components/GraphOne';
+import FooterCM from '../components/FooterCM';
 
 const HomePage = () => {
     return (
-        <Layout pageTitle="Project Break The Cycle - Sustainably Ending Poverty">
-          <NavOne />
-          <SliderOne />
-          <ServiceArea />
-          <GoodPartners />
-          <CampaignArea />
-          {/* <GraphOne /> */}
-          <RaisingMoney />
-          
-          <CallToAction />
-          <CallToActionTwo />
-          <Footer />
-        </Layout>
+        //<Layout pageTitle="Project Break The Cycle - Sustainably Ending Poverty">
+
+          <div> <Footer /> 
+          <FooterCM /> </div>
+
+        //</Layout>
     );
 };
 
