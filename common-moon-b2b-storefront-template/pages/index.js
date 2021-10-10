ae357ff259c2b2d22fa4dc3d71884f875abc16d1
Archna-1 <archnas2@illinois.edu>
2021-10-10 18:44:19 -0500

    Add Landing Page

---
+++ common-moon-b2b-storefront-template/pages/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Footer from "../components/Footer";
+import FooterCM from "../components/FooterCM";
+import LandingPage from "../components/LandingPage";
+
+const HomePage = () => {
+  return (
+    //<Layout pageTitle="Project Break The Cycle - Sustainably Ending Poverty">
+
+    <div>
+      {" "}
+      <LandingPage />
+      <Footer />
+      <FooterCM />
+    </div>
+
+    //</Layout>
+  );
+};
+
+export default HomePage;
